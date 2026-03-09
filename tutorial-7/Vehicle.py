32616d59400ccd4c798b62c9986826b319d6e883
Vinisha Manek <Vinisha@exoshop.com>
2026-03-09 11:33:22 -0400

    tut7: add inheritance example

---
+++ tutorial-7/Vehicle.py
@@ -0,0 +1,9 @@
+# create a Vehicle class with
+# max_speed and mileage as instance attribute
+
+# class Vehicle:
+
+# create a child class Bus that will inherit 
+# all variables and methods rom the Vehicle class
+
+# class Bus:
