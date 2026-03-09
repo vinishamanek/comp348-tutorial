636436dbfaa7c2f7ca59fe5e62024059163c6194
Vinisha Manek <Vinisha@exoshop.com>
2026-03-09 15:46:08 -0400

    tut6: add platform example

--- tutorial-7/Vehicle.py
+++ tutorial-7/Vehicle.py
@@ -1,9 +1,8 @@
 # create a Vehicle class with
 # max_speed and mileage as instance attribute
 
-# class Vehicle:
-
-# create a child class Bus that will inherit 
+# create a child classes Bus and Car that will inherit 
 # all variables and methods rom the Vehicle class
 
-# class Bus:
+# add a property that every sincle class instance should have
+# let this property be the class attribute color, with default value white
